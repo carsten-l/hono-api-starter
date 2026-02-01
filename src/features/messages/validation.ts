21aa51186f8ae04547954e050e9c46844b85fefc
clurts <clu@rts.dk>
2026-02-01 08:15:03 +0100

    cleanup messages

--- src/features/messages/validation.ts
+++ src/features/messages/validation.ts
@@ -1,9 +1,13 @@
 import { z } from 'zod'
 
 export const messageSchema = z.object({
+  id: z.number(),
   name: z.string().min(1, "Name is required"),
   email: z.string().email("Invalid email format"),
   subject: z.string().min(1, "Subject is required"),
   message: z.string().min(1, "Message is required"),
 });
+
+export type Message = z.infer<typeof messageSchema>;
+export type NewMessage = Omit<Message, 'id'> ;
 
