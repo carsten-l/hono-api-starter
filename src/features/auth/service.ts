b28c2ec5aae3ef8ff180f128e8ac18ce6fd80a92
clurts <clu@rts.dk>
2026-02-09 08:48:06 +0100

    refactor: improve error handling in auth API and service

--- src/features/auth/service.ts
+++ src/features/auth/service.ts
@@ -1,16 +1,16 @@
 import type { NewUser, LoginUser } from "../users/validation";
 import prisma, { Prisma } from "../../core/db";
+import { HTTPException } from 'hono/http-exception'
 import { hashSync, compareSync } from "bcryptjs";
 import { sign } from "hono/jwt";
 
 
     export async function createUser (userData: NewUser) {
-        const prismaData: Prisma.UserCreateInput = {
+        const user = await prisma.user.create({ data: {
             name: userData.name,
             email: userData.email,
             password: hashSync(userData.password, 15)
-        }
-        const user = await prisma.user.create({ data:prismaData });
+        }});
         const { password, ...userWithoutPassword } = user;
         return userWithoutPassword;
     }
@@ -20,20 +20,25 @@ import { sign } from "hono/jwt";
             where: { email: body.email },
         });
 
-        if (user && compareSync(body.password, user.password)) {
-            let accessToken = await sign({
-                id: user.id,
-                exp: Math.floor(Date.now() / 1000) + 60 * 60,
-            }, process.env.JWT_SECRET as string
-            );
-            return {
-                id: user.id,
-                name: user.name,
-                accessToken,
-                validUntil: Date.now() + (60 * 60 * 1000)
-            };
-        } else {
-            throw new Error("Invalid credentials");
+        if (!user || !compareSync(body.password, user.password)) {
+            throw new HTTPException(401, {
+                message: "Invalid credentials",
+                cause: { formErrors: ["Invalid email or password"] } // optional, frontend-friendly
+            });
         }
+
+        const accessToken = await sign(
+            { id: user.id, exp: Math.floor(Date.now() / 1000) + 60 * 60 },
+            process.env.JWT_SECRET as string
+        );
+
+        
+        return {
+            id: user.id,
+            name: user.name,
+            accessToken,
+            validUntil: Date.now() + (60 * 60 * 1000)
+        };
+       
     }
 
